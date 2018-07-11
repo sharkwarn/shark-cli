d16d3af57d6708eb199e1f978fcc1ca7729d65a1
guanyu <zhuliangcan@foresee.com.cn>
2018-07-11 14:12:17 +0800

    update

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict'
+
+var program = require('commander');
+program
+  .version(require('../package').version );
+
+// 定义使用方法
+program
+  .usage('<command>')
+
+program
+  .command('init')
+  .description('init a node + es6 template')
+  .alias('i')
+  .action(function(){
+      console.log('init')
+  })
+program.parse(process.argv)
+
+if(!program.args.length){
+  program.help()
+}
