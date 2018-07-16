d5c3b1683cc2fea16588769495976fb031d9b70b
guanyu <zhuliangcan@foresee.com.cn>
2018-07-16 13:10:21 +0800

    update

--- test/file.js
+++ test/file.js
@@ -20,6 +20,36 @@ function createDir(path) {
 
 // copyFile('./spawn.js', './xiaowu/a.js')
 
-fs.copyFile('./xiaowu.js', './xiaowu2.js', (err) => {
-    console.log(err)
-})
+// fs.copyFile('./xiaowu.js', './xiaowu2.js', (err) => {
+//     console.log(err)
+// })
+function copyFiles (path, origin) {
+    const files = fs.readdirSync(path)
+    for( let i=0; i<files.length; i++ ) {
+        const childPath = `${path}/${files[i]}`
+        const file = fs.statSync(childPath)
+        if( file.isFile() ) {
+            let targetPath = origin + '/' + childPath
+            const arr = targetPath.split('/')
+            let single = ''
+            arr.forEach(item => {
+                if( item == '.' ){
+                }else {
+                    const currentPath = origin + single + '/' + item
+                    single = single + '/' + item
+                    if( !fs.existsSync(currentPath) ) {
+                        console.log(currentPath)
+                        fs.mkdirSync(currentPath)
+                    }
+                }
+            })
+            fs.copyFileSync(childPath, targetPath)
+        }else {
+            copyFiles(childPath, origin)
+        }
+
+    }
+}
+
+copyFiles('./cope', './xiaowu2')
+
